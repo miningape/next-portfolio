6a707766651c6d729c6446a6eee646e5551d798a
Kyle Johnson <miningape@gmail.com>
2023-04-08 00:53:27 +0200

    added + configured particles.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kyle - hermetic.dev</title>
+        <meta
+          name="description"
+          content="Kyle Johnson's Portfolio - hermetic.dev"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
