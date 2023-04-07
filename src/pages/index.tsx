fd752c1b1a993c24e379e89cf4935e48c41c8c32
Kyle Johnson <miningape@gmail.com>
2023-04-08 01:52:36 +0200

    Added name + links for github and linkedin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
 import { type NextPage } from "next";
-import { useCallback } from "react";
+import React, { useCallback } from "react";
 import { Particles } from "react-particles";
 import { loadFull } from "tsparticles";
 import { type Engine } from "tsparticles-engine";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+import { type IconType } from "react-icons";
+
+const LinkWithIcon: React.FC<{
+  Icon: IconType;
+  href: string;
+  text: string;
+}> = ({ Icon, href, text }) => (
+  <Link
+    href={href}
+    className="flex px-10 text-lg text-sky-100 hover:text-amber-100"
+  >
+    <Icon className="mr-1 h-7 w-7 align-middle" />
+    <span>{text}</span>
+  </Link>
+);
 
 const Home: NextPage = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
@@ -12,7 +29,26 @@ const Home: NextPage = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-bl from-[#1f1e1e] to-[#0b0b0c]">
+      <div>
+        <p className="text-8xl text-sky-100">Kyle Johnson</p>
+
+        <div className="flex justify-center pt-5">
+          <LinkWithIcon
+            Icon={FaGithub}
+            href="https://github.com/miningape"
+            text="Github"
+          />
+
+          <LinkWithIcon
+            Icon={FaLinkedin}
+            href="https://linkedin.com/in/kyle-craig-johnson/"
+            text="Linkedin"
+          />
+        </div>
+      </div>
+
       <Particles
+        className="absolute"
         init={particlesInit}
         options={{
           background: {
